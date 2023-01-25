1b6bd6439645f3d309ac2114c1842ae9ba1ca654
Litvinov Nikita <chelovek.shp@gmail.com>
2023-01-25 19:00:06 +0300

    Router Added

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).use(VueAxios, axios)
   .$mount('#app')
